f3faeb7b655b530a953b5fb4648bda71d0688ef8
SihamBen <siham.bendaia37@gmail.com>
2020-12-25 17:47:28 +0100

    added basic features

---
+++ components/Navbar.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  function handleClick(e)
+  {console.log("hf")
+    var nav = document.getElementsByTagName("NAV")[0];
+    nav.classList.toggle('active');
+  }
+    return (
+        <header className=" flex flex-row justify-between align-center md:absolute md:top-0 w-full  z-50  px-6 md:my-0 sm:my-4 my-2 h-12 ">
+          <div className="flex flex-row  justify-between w-full md:w-1/2 " >
+            <div className=""> <img alt="logo" src="logo.png" className="max-h-full  object-cover"></img></div>
+         
+            <div className="hidden md:flex flex-row   justify-between w-full md:w-1/2 px-8  items-end  ">
+            <a href="https://www.instagram.com/s7aba_podcast/" target="_blank"><img src="./instagram.svg" className="w-7"/></a>
+            <a href="https://twitter.com/s7aba_podcast/" target="_blank"><img src="./twitter.svg" className="w-11 self-center "/></a>
+            <a href="https://open.spotify.com/show/1BXjXjq5nn1SGlU23i6wf3?si=ZqP78Ke3Qs2_SSSLKI4fCg" target="_blank"><img src="./spotify.svg" className="w-7"/></a>
+            <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy84ZGE2OTJjL3BvZGNhc3QvcnNz?sa=X&ved=0CAIQ9sEGahcKEwjYg8KBg87tAhUAAAAAHQAAAAAQBA" target="_blank"><img className="w-7" src="./google-podcasts.svg"/></a>
+              </div>
+              <button className="md:hidden self-end " onClick={handleClick}><i className=" fas fa-bars font-extrabold text-gray-700 text-3xl "></i></button>
+            </div>
+            
+          <nav className="absolute md:relative md:w-1/2 hidden md:block bg-white md:bg-transparent h-screen w-screen z-50">
+         
+            <ul className="p-4   flex flex-col md:flex-row md:justify-around md:items-start justify-between  items-center   h-full ">
+            <button className="md:hidden self-end" onClick={handleClick}><i className=" fas fa-times text-3xl"></i></button>
+              <li>
+                <Link href="#home" >
+                  <button className= "font-semibold text-gray-700 md:text-white focus:outline-none hover:text-gray-200" onClick={handleClick}>Home</button>
+                </Link>
+              </li>
+              <li> <Link href="#about">
+                  <button className="font-semibold text-gray-700 md:text-white focus:outline-none   hover:text-gray-200 " onClick={handleClick}>About Us</button>
+                </Link></li>
+              <li> <Link href="#episodes">
+                  <button className="font-semibold text-gray-700 md:text-white focus:outline-none hover:text-gray-200" onClick={handleClick}>Episodes</button>
+                </Link></li>
+              <li> <Link href="#contact">
+                  <button className="font-semibold text-gray-700 md:text-white focus:outline-none  hover:text-gray-200" onClick={handleClick}>Contact Us</button>
+                </Link></li>
+               
+            <a href="https://www.instagram.com/s7aba_podcast/" target="_blank" className="md:hidden"><img src="./instagram.svg" className="w-7"/></a>
+            <a href="https://twitter.com/s7aba_podcast/" target="_blank" className="md:hidden"><img src="./twitter.svg" className="w-11 self-center "/></a>
+            <a href="https://open.spotify.com/show/1BXjXjq5nn1SGlU23i6wf3?si=ZqP78Ke3Qs2_SSSLKI4fCg" target="_blank" className="md:hidden"><img src="./spotify.svg" className="w-7"/></a>
+            <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy84ZGE2OTJjL3BvZGNhc3QvcnNz?sa=X&ved=0CAIQ9sEGahcKEwjYg8KBg87tAhUAAAAAHQAAAAAQBA" target="_blank" className="md:hidden"><img className="w-7" src="./google-podcasts.svg"/></a>
+              
+            </ul>
+          </nav>
+        </header>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,51 @@
+import Navbar from '../components/Navbar'
+import Link from 'next/link'
+import About from "../components/About"
+import Episode from "../components/Episode"
+import Contact from "../components/Contact"
 import { getEpisodes } from "../content/episodes"
 
-function HomePage({ episodes }) {
-  return <div className="flex flex-wrap md items-center h-screen">
-    <div className="bg-white w-full md:w-1/2 h-screen">
-      <div className="mx-32">
-        <h1 className="text-6xl font-bold mt-16">S7aba Podcast</h1>
 
-        <div className="flex mt-16 font-light text-gray-500">
-          <div className="pr-4">
-            <span className="uppercase">Minimalist Cloud Podcast</span>
-            <p className="text-2xl text-gray-900 font-semibold pt-2">Power by Moroccan Darija & Tea</p>
-          </div>
-        </div>
-
-        <div
-          className="description w-full sm: md:w-2/3 mt-16 text-gray-500 text-sm"
-        >
-          Some description here
-          {JSON.stringify(episodes)}
-        </div>
-
-        <button className="uppercase mt-5 text-sm font-semibold hover:underline">
-          read more
-      </button>
+function HomePage({episodes}) {
+  return (
+     <div >
+      <div className="flex flex-wrap  md items-center md:h-screen relative justify-center  ">
+      <Navbar></Navbar>
+      <div className=" h-1/3 md:w-1/2 flex flex-col  items-center content-center justify-between py-20 md:py-0 order-3 md:order-none" id="home">
+            <div className="text-center">
+              <span className="uppercase ">Minimalist Cloud Podcast</span>
+              <p className="text-3xl  font-bold pt-2 gradient-text">
+                Powered by Moroccan Darija & Tea
+              </p>
+            </div>
+          <Link href="#about">
+       <button className="hidden md:block border-solid border-blue-300 border-2 rounded-full px-3 py-1 focus:outline-none  hover:border-blue-200 my-10"><i className="fas fa-arrow-down text-xl text-blue-300"></i></button>
+       </Link>
+        
+      </div >
+      <div className=" w-full md:w-1/2 md:h-screen relative bg-red-200 ">
+      <h1 className=" text-4xl sm:text-6xl font-bold mt-16 absolute  top-2 md:top-32 sm:top-20  left-4 md:left-2 text-white z-20">S7aba Podcast</h1>
+      <h1 className="text-4xl sm:text-6xl font-bold mt-16 absolute top-12  md:top-52  sm:top-36 right-4 md:right-2 text-white z-20">سحابة بودكاست</h1>
+        <img
+          src="https://source.unsplash.com/_alEmiTYyYk/1600x900"
+          className="h-full md:h-screen w-full filter-grayscale object-fit"
+          alt=""
+        />
       </div>
     </div>
-    <div className="bg-red-600 w-full md:w-1/2 h-screen">
-      <img
-        src="https://source.unsplash.com/_alEmiTYyYk/1600x900"
-        className="h-screen w-full"
-        alt=""
-      />
+    
+  <About></About>  
+<section id="episodes" className="flex flex-col items-center"> {episodes.map((episode)=>{
+  return <Episode episode={episode} key={episode.guid}></Episode>
+})}</section>
+<Contact></Contact>
     </div>
-  </div>
+  );
 }
-
 HomePage.getInitialProps = async (ctx) => {
   const episodes = await getEpisodes();
   return { episodes }
 }
 
-export default HomePage
+
+export default HomePage;
