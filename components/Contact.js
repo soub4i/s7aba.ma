2a459b4ee3107d8b2e398c5b3e48d5ea1459e56e
Abderrhim Soubai <gx.coder@gmail.com>
2021-01-19 21:50:50 +0100

    fix(page): tiny fix

--- components/Contact.js
+++ components/Contact.js
@@ -7,11 +7,11 @@ export default function Contact() {
                 <h3 className="font-medium text-gray-600 mb-2">Or fill the contact form down below</h3>
             </div>
              <form className="flex flex-col w-2/3 md:w-1/2"  >
-            <label for="name" className="font-semibold text-gray-500 mb-2 " >Full Name</label>
+            <label htmlFor="name" className="font-semibold text-gray-500 mb-2 " >Full Name</label>
             <input type="text" className="mb-6 h-8 shadow-lg  rounded focus:outline-none focus:ring focus:border-blue-300"  name="name" required />
-            <label for="email" className="font-semibold text-gray-500 mb-2 ">Email</label>
+            <label htmlFor="email" className="font-semibold text-gray-500 mb-2 ">Email</label>
             <input type="email" className="mb-6 h-8 shadow-lg  rounded focus:outline-none focus:ring focus:border-blue-300" name="email" required />  
-            <label for="msg" className="font-semibold text-gray-500 mb-2">Message</label>
+            <label htmlFor="msg" className="font-semibold text-gray-500 mb-2">Message</label>
             <input type="text" className="  mb-6  h-28 shadow-lg  rounded focus:outline-none focus:ring focus:border-blue-300" name="msg" />
             <button type="submit" className="p-1 self-center w-3/4 md:w-1/4 font-semibold text-blue-300 border-solid border-blue-300 border-2 rounded focus:outline-none focus:ring focus:border-blue-300 "><i className="far fa-paper-plane pr-1"></i> Send </button>
           </form>
